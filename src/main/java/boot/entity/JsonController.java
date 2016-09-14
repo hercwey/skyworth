c9e4d677f4606bb9e60d4c41919613862c3d4ae2
zxfandff <10032800@qq.com>
2016-09-14 15:53:29 +0800

    spring boot json

--- src/main/java/boot/entity/JsonController.java
+++ src/main/java/boot/entity/JsonController.java
@@ -1,21 +1,27 @@
-package boot;
+package boot.entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class HwController {
+public class JsonController {
 	
-	@RequestMapping("/")
-	public String hello(){
-		return "Hello world";
+	@RequestMapping("/getUser")
+	@ResponseBody
+	public User getUser(){
+		User u = new User();
+		
+		u.setName("zxf");
+		u.setAge("34");
+		return u;
 		
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(HwController.class, args);
+		SpringApplication.run(JsonController.class, args);
 	}
 }
