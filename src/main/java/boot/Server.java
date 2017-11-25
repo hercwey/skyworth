295eb3120e538197b2d804fefa79091abc4bbcca
zhouxiaofeng1 <zhouxiaofeng1@jd.com>
2017-11-25 11:26:46 +0800

    测试提交

--- src/main/java/boot/Server.java
+++ src/main/java/boot/Server.java
@@ -1,11 +1,8 @@
 package boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
@@ -22,20 +19,21 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 //}
 @SpringBootApplication
 @ServletComponentScan
-public class Server extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {  
+@MapperScan("boot.repository")
+public class Server /**extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer*/ {
 	  
-    @Override  
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
-        return builder.sources(Server.class);  
-    }  
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(Server.class);
+//    }
       
-    public static void main(String[] args) {  
-        SpringApplication.run(Server.class, args);  
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
     }  
   
   
-    @Override  
-    public void customize(ConfigurableEmbeddedServletContainer container) {  
-        container.setPort(7890);  
-    }  
+//    @Override
+//    public void customize(ConfigurableEmbeddedServletContainer container) {
+//        container.setPort(7890);
+//    }
 } 
